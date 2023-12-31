966d008c09a4e3ce7ce619c0ad72ac20e3ad4ffb
JP007-Star <justice_of_peace@yahoo.com>
2024-01-01 00:38:29 +0530

    MajorityElement

---
+++ src/scalar/MajorityElement.java
@@ -0,0 +1,59 @@
+package scalar;
+public class MajorityElement {
+    public static int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        // Step 1: Find the candidate
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        // Step 2: Validate the candidate
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        // The candidate is the majority element
+        if (count > nums.length / 2) {
+            return candidate;
+        }
+
+        // In case there is no majority element (this should not happen based on the problem statement)
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {3, 2, 3};
+        System.out.println(majorityElement(nums1)); // Output: 3
+
+        int[] nums2 = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(majorityElement(nums2)); // Output: 2
+    }
+}
+
+
+    /*
+    *
+
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+    * https://leetcode.com/problems/majority-element/*/
+
