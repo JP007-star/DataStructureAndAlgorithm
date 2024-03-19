3550afbb82e5cb031cb281b348f480b9580da11e
JP007-Star <justice_of_peace@yahoo.com>
2024-03-19 20:51:01 +0530

    IntersectionOfArray

--- src/dsa/array/IntersectionOfArray.java
+++ src/dsa/array/IntersectionOfArray.java
@@ -1,8 +1,6 @@
 package dsa.array;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class IntersectionOfArray {
     public static void main(String[] args) {
@@ -16,6 +14,18 @@ public class IntersectionOfArray {
         int[] nums3 = {4, 9, 5};
         int[] nums4 = {9, 4, 9, 8, 4};
         System.out.println(Arrays.toString(intersection(nums3, nums4)));  // Output: [9, 4]
+
+        // Example 1
+        int[] nums_1 = {1, 2, 2, 1};
+        int[] nums_2 = {2, 2};
+        int[] intersection1 = intersect(nums_1, nums_2);
+        System.out.println("Example 1: " + Arrays.toString(intersection1)); // Output: [2, 2]
+
+        // Example 2
+        int[] nums_3 = {4, 9, 5};
+        int[] nums_4 = {9, 4, 9, 8, 4};
+        int[] intersection2 = intersect(nums_3, nums_4);
+        System.out.println("Example 2: " + Arrays.toString(intersection2));
     }
     public static int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
@@ -46,6 +56,31 @@ public class IntersectionOfArray {
         int index = 0;
         for (int num : intersection) {
             result[index++] = num;
+        }
+
+        return result;
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        // Map to store frequency of elements in nums1
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums1) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> intersectionList = new ArrayList<>();
+        // Iterate through nums2 and check for common elements
+        for (int num : nums2) {
+            if (frequencyMap.containsKey(num) && frequencyMap.get(num) > 0) {
+                intersectionList.add(num);
+                frequencyMap.put(num, frequencyMap.get(num) - 1);
+            }
+        }
+
+        // Convert ArrayList to array
+        int[] result = new int[intersectionList.size()];
+        for (int i = 0; i < intersectionList.size(); i++) {
+            result[i] = intersectionList.get(i);
         }
 
         return result;
