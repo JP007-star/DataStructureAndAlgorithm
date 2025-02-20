05c9e8f630805b35539849036331fcffa06070e7
jayaprasad.m <justice_of_peace@yahoo.com>
2025-02-20 17:34:37 +0530

    search updated

--- src/Search.java
+++ src/Search.java
@@ -1,6 +1,6 @@
 /*
  * @project DataStructureAndAlgorithm
- * @fileName LinearSearch
+ * @fileName Search
  * @author Jaya Prasad.M --> jaya_muthukrishnan
  * @email jaya_muthukrishnan@thbs.com
  * @date 14 12 2021 06:23 PM
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-public class LinearSearch {
+public class Search {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
@@ -36,4 +36,45 @@ public class LinearSearch {
         }
         return -1;
     }
+
+
+    private static int binarySearch(int[] arr, int target) {
+        int startIndex=0;
+        int endIndex=arr.length;
+
+        while(startIndex<=endIndex){
+            //0          6        0
+            int pivot=startIndex+(endIndex-startIndex)/2;
+
+            if(pivot<= arr.length-1) {   //-->  eliminating array out of bound exception  when call number not in array
+
+                if (arr[pivot] == target) return pivot;
+
+                if (arr[pivot] < target) startIndex = pivot + 1;
+                else endIndex = pivot - 1;
+            }
+            else return -1;
+
+
+        }
+        return -1;
+    }
 }
+
+
+
+/*
+
+ while (left <=right)
+
+
+ mid =left+(right-left)/2
+
+ if(nums[mid]== target)  return mid
+
+ if nums[mid] <=target  left=pivot+1
+
+ else  right=mid-1;
+
+
+ */
